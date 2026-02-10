12077a695ee875a93c3e5c3c08c0b96356f7c082
matmar435 <matmar435@gmail.com>
2026-02-10 18:31:11 +0100

    path modification

--- app/load_knowledge.py
+++ app/load_knowledge.py
@@ -1,6 +1,6 @@
 from app.services.vector_store import  add_documents
 
-with open("app/data/knowledge.txt", "r", encoding="utf-8") as f:
+with open("data/knowledge.txt", "r", encoding="utf-8") as f:
     texts = [line.strip() for line in f.readlines() if line.strip()]
 
 add_documents(texts)
