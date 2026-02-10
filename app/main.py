d5ae6705d894cd3ad4b65fb820c4ac94dab6d8c0
matmar435 <matmar435@gmail.com>
2026-02-10 12:26:25 +0100

    registration chat

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.api import healt
+from app.api import healt, chat
 
 app = FastAPI(title="Ai Knowledge Assistant")
 
 #rejestracja routerów
 app.include_router(healt.router, prefix="/health")
+app.include_router(chat.router, prefix="/chat")
