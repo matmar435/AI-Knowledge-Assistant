f6333b33ef21acdba9ccd6b3a7f881f4d90f0c31
matmar435 <matmar435@gmail.com>
2026-02-10 16:11:36 +0100

    add load_knowledge

---
+++ app/load_knowledge.py
@@ -0,0 +1,8 @@
+from app.services.vector_store import  add_documents
+
+with open("app/data/knowledge.txt", "r", encoding="utf-8") as f:
+    texts = [line.strip() for line in f.readlines() if line.strip()]
+
+add_documents(texts)
+
+print("Wiedza załadowana")
