c6de420f2191d117f6387d14884dd2180c87c812
matmar435 <matmar435@gmail.com>
2026-02-11 17:36:33 +0100

    add router documents in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.api import healt, chat
+from app.api import healt, chat, documents
 
 app = FastAPI(title="Ai Knowledge Assistant")
 
-#rejestracja routerów
+# rejestracja routerów
 app.include_router(healt.router, prefix="/health")
 app.include_router(chat.router, prefix="/chat")
+app.include_router(documents.router, prefix="/documents", tags=["documents"])
